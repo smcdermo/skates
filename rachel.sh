0cdfa085c7948dc37f6c11b8a2241df1d8e9a6ca
susanne.mcdermott <susanne.mcdermott@noaa.gov>
2016-03-03 10:14:34 -0800

    initial commit and changes in rachel

--- rachel.sh
+++ rachel.sh
@@ -1,6 +1,7 @@
 #this is a datafile for practice in GIT"
 #calcualte redcued states for data files
 for datafile in "$@" 
+#here is another comment just for the sake of it
 do 
 echo $datafile
 bash goostats -J 100 -r $datafile stats -$datafile
